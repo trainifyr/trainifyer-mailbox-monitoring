c104046c697349dce7250b8ccb7272f5035303c8
Estato Admin <admin@estato.com>
2026-09-12 12:29:10 +0530

    chore: remove unused supabaseAnonClient, tmp scripts, logs, and empty App.css

--- tmp/check_db.js
+++
@@ -1,4 +0,0 @@
-const axios = require('axios');
-axios.get('http://localhost:5000/api/health/db')
-  .then(res => console.log(JSON.stringify(res.data, null, 2)))
-  .catch(err => console.error(JSON.stringify(err.response.data, null, 2)));
--- tmp/print_issues.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const d = fs.readFileSync('tmp/gh_issues.json', 'utf8');
-const j = JSON.parse(d);
-j.forEach(i => console.log(i.number + ': ' + i.title));
